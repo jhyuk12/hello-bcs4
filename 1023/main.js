29a212a7021c5972842527c277a4a07579453465
unknown <jh.jang@outlook.com>
2023-10-23 17:51:20 +0900

    Add my name

--- 1023/main.js
+++ 1023/main.js
@@ -1,1 +1,9 @@
 console.log("Hellow, BCS4!");
+
+const myName = document. getElementById("my-name");
+
+const h3 = document.createElement("h3");
+
+h3.innerText = "My name is h662.";
+
+myName.appendcChild(h3);
